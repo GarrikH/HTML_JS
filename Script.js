c9350a78e24cf2ded5f7880f05f19485aced1d3d
Garrik Hoyt <35749221+GarrikH@users.noreply.github.com>
2020-10-22 11:07:33 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,89 @@
+function section1Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "inline-block";
+			s2.style.display = "none"; 
+			s3.style.display = "none";
+			s4.style.display = "none";
+			s5.style.display = "none";
+			s6.style.display = "none";
+		}
+		
+		function section2Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "none";
+			s2.style.display = "inline-block"; 
+			s3.style.display = "none";
+			s4.style.display = "none";
+			s5.style.display = "none";
+			s6.style.display = "none";
+		}
+		
+		function section3Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "none";
+			s2.style.display = "none"; 
+			s3.style.display = "inline-block";
+			s4.style.display = "none";
+			s5.style.display = "none";
+			s6.style.display = "none";
+		}
+		
+		function section4Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "none";
+			s2.style.display = "none"; 
+			s3.style.display = "none";
+			s4.style.display = "inline-block";
+			s5.style.display = "none";
+			s6.style.display = "none";
+		}
+		
+		function section5Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "none";
+			s2.style.display = "none"; 
+			s3.style.display = "none";
+			s4.style.display = "none";
+			s5.style.display = "inline-block";
+			s6.style.display = "none";
+		}
+		
+		function section6Click() {
+			var s1 = document.getElementById("Section1");
+			var s2 = document.getElementById("Section2");
+			var s3 = document.getElementById("Section3");
+			var s4 = document.getElementById("Section4");
+			var s5 = document.getElementById("Section5");
+			var s6 = document.getElementById("Section6");
+			s1.style.display = "none";
+			s2.style.display = "none"; 
+			s3.style.display = "none";
+			s4.style.display = "none";
+			s5.style.display = "none";
+			s6.style.display = "inline-block";
+		}
